244fca591788b8032e83feb1ad02a1446ba40695
Ahmad Wirayuda <wirayudha@impstudio.co.id>
2024-04-23 15:55:50 +0700

    copy ownership as function

--- src/main.rs
+++ src/main.rs
@@ -33,4 +33,18 @@ fn main() {
     println!("{}", c);
     println!("{}", c2);
 
+    //ownership and function
+    let sf = String::from("hello");
+    takes_ownership(sf);
+
+    let i = 5;
+    makes_copy(i);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
 }
