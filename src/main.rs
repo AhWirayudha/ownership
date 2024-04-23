aefc6f853785c02918ef83b92931f9cace59e42e
Ahmad Wirayuda <wirayudha@impstudio.co.id>
2024-04-23 15:25:58 +0700

    clone

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,11 @@ fn main() {
     // println!("{}", s1); error caused by ownership already moved to s2
     // println!("{}, world!", s1);
     println!("{}, world! s2", s2);
+
+    // u can use clone if necessary but it's not recommended due to cost expensive
+    let s3 = String::from("hello");
+    let s4 = s3.clone();
+    println!("s3 = {}, s4 = {}", s3, s4);
+
+    
 }
